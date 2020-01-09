f93cbc3117c05b57c5bf02579bcf19cca8e9ec7a
Anna Pawlik <pawlikanna24@gmail.com>
2020-01-09 10:21:38 +0100

    Add styles HomePage

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles/global.scss';
 
 const Root = () => (
   <Provider store={store}>
