6095e8c5b770c88d4e1b85e9310b98cbde48d066
Anna Pawlik <pawlikanna24@gmail.com>
2020-01-16 10:49:35 +0100

    Change NavBar

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { NavLink } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../Logo/Logo';
 import './NavBar.scss';
 
-class NavBar extends React.Component {
+const NavBar = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
 
-  render() {
-    return (
-      <div className="navbar text-uppercase">
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <div className="navbar-desktop text-uppercase">
         <Logo />
         <div className="navbar navbar-links">
           <NavLink to='/'>Home</NavLink>
@@ -21,9 +25,35 @@ class NavBar extends React.Component {
           </NavLink>
         </div>
       </div>
-    );
-  }
-
+      <div className="navbar-mobile">
+        <Navbar color="faded" light>
+          <Logo />
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink to='/'>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/FAQ'>FAQ</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/termsconditions'>Terms & Conditions</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/contact'>Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/basket'>
+                  <FontAwesomeIcon icon={faShoppingBag} />
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
