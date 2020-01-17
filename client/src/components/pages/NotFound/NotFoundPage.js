abd720a313fddb82512b75328a00d56c89ab75b3
Anna Pawlik <pawlikanna24@gmail.com>
2020-01-17 08:16:43 +0100

    Add NotFoundPage

--- client/src/components/pages/NotFound/NotFoundPage.js
+++ client/src/components/pages/NotFound/NotFoundPage.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.scss';
 
 const NotFoundPage = () => (
-    <div>
-        <h1>404 Page Not Found</h1>
+    <div className="not-found">
+        <h1>Oops!</h1>
+        <h3>Sorry, the page not found</h3>
+        <p>The link you followed probably broken or the page has been removed.</p>
+        <Link to='/' className="not-found-btn">Back to home</Link>
     </div>
 );
 
